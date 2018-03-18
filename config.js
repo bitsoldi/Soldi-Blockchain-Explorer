c7ca5f15068e7749c34631ac7fa09842163d485f
Brondo76 <35544655+Brondo76@users.noreply.github.com>
2018-03-18 18:16:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://212.237.7.129:8117';
+var api = 'http://212.237.7.129:8081';
 var blockTargetInterval = 240;
 var coinUnits = 1000000000000;
 var symbol = 'SDI';
@@ -6,8 +6,8 @@ var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
     "sdi": [
-		["mine.klondikepool.com", "http://212.237.7.129:8082"],
-		["soldi2.com", "http://94.177.187.216:8082"]		
+		["mine.klondikepool.com", "http://212.237.7.129:8080"],
+		["soldi2.com", "http://94.177.187.216:8080"]		
 		
 		
     ]
